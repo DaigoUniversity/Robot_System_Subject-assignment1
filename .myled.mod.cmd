95e6452fe6ae9ec1594069c3b8d97e3ddf8cf6d7
Daigo Takano <s20C1067CT@s.chibakoudai.jp>
2021-12-17 13:21:05 +0900

    Delete .myled.mod.cmd

--- .myled.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/Robot_System_Subject-assignment1/myled.mod := { echo  /home/ubuntu/Robot_System_Subject-assignment1/myled.o;  echo; } > /home/ubuntu/Robot_System_Subject-assignment1/myled.mod
