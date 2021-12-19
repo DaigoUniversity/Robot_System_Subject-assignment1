642d6658505fbd7dc873b4b02588a0e8d5e4bd5e
Daigo Takano <s20C1067CT@s.chibakoudai.jp>
2021-12-19 20:48:42 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -10,7 +10,7 @@
 #include <linux/uaccess.h>
 #include <linux/io.h>
 
-MODULE_AUTHOR("Daigo Takano + Ryuichi Ueda");
+MODULE_AUTHOR("Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
